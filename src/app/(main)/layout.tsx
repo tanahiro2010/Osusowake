5934c688931f8c645b307d930fa001a3b570d8d3
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-14 19:59:35 +0900

    edit: spaceを追加

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactProps } from "@/types/react";
 export default function MainLayout({ children }: ReactProps) {
   return (
     <div className="bg-[#f8efde]">
-        {children}
+      { children }
     </div>
   );
 }
