be979ef5721bbd6df683d1d0b9092923cdd799b7
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-15 15:59:02 +0900

    add: Amazonのトラッカー情報更新APIエンドポイントを作成

---
+++ src/app/api/users/tracker/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest } from "next/server";
+import { ProfileInputSchema, PaginationQuerySchema } from "@/types/zod/prisma/profile";
+import { prisma } from "@/lib/prisma";
+import { withAuth } from "@/lib/middleware";
+import { badRequest, ok } from "@/lib/helpers/response";
+
+
+const POST = (async (req: NextRequest) => withAuth(req, (async (auth) => {
+    // JSONをパース
+    const body = await req.json();
+    const trackerId = body.trackerId;
+    if (typeof trackerId !== 'string') {
+        return badRequest("trackerIdは必須です。");
+    }
+
+    try {
+        const result = await prisma.amazonTracker.upsert({
+            where: { userId: auth.user.id },
+            create: {
+                trackerId: trackerId,
+                user: { connect: { id: auth.user.id } },
+            },
+            update: {
+                trackerId,
+            },
+        });
+
+        return ok(result, "トラッカー情報が更新されました。");
+    } catch (e) {
+        console.error(e);
+        return badRequest("トラッカー更新中にエラーが発生しました。");
+    }
+})));
+
+export { POST };
