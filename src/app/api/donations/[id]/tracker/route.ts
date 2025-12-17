a060be60b6f282fa0ad483dec2a40898bebc39a2
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-17 18:42:49 +0900

    add: おすそ分け時、メール送信するように設定

--- src/app/api/donations/[id]/tracker/route.ts
+++ src/app/api/donations/[id]/tracker/route.ts
@@ -14,17 +14,39 @@ const POST = (async (req: NextRequest, context: DonationPageContext) => {
     const token = body?.token;
 
     if (!token) return unauthorized('Invalid token');
-    const payload = await verifyJwt(token);
+    const [payload, user] = await Promise.all([
+        verifyJwt(token),
+        prisma.user.findFirst({
+            where: { AmazonTracker: { productLinks: { some: { id } } } },
+            select: { id: true, email: true, name: true }
+        })
+    ]);
     if (!payload) return unauthorized('Invalid token');
 
     try {
-        const history = await prisma.linkUseHistory.create({
-            data: {
-                productLink: { connect: { id } }
-            },
+        const [history] = await Promise.all([
+            prisma.linkUseHistory.create({
+                data: {
+                    productLink: { connect: { id } }
+                },
 
-            select: { productLink: true, id: true, usedAt: true }
-        });
+                select: { productLink: true, id: true, usedAt: true }
+            }),
+            fetch(process.env.MAIL_API_ENDPOINT ?? "", {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    token: process.env.MAIL_API_TOKEN ?? '',
+                    to: user?.email ?? '',
+                    subject: '【おすそわけ】あなたにおすそわけがありました',
+                    body: `こんにちは、${user?.name ?? 'ユーザー'}さん。<br>` +
+                        `あなたへの寄付リンクが使用されました。<br><br>` +
+                        `寄付リンクURL: <a href="${process.env.NEXT_PUBLIC_APP_URL}/donations/${id}">${process.env.NEXT_PUBLIC_APP_URL}/donations/${id}</a><br>` +
+                        `使用日時: ${new Date().toLocaleString()}<br><br>` +
+                        `ご使用ありがとうございました。<br><br>`
+                })
+            })
+        ]);
         return ok({ history }, "Donation link usage tracked");
     } catch (error) {
         console.error("Error tracking donation link usage:", error);
