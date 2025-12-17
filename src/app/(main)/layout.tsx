13c033371522b135c4d073e8365e7f6c91929056
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-17 15:33:59 +0900

    fix: レスポンシブ対応

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,9 +3,9 @@ import { ReactProps } from "@/types/react";
 export default function MainLayout({ children }: ReactProps) {
   return (
     <div className="bg-[#f8efde]">
-      <main className="flex min-h-screen flex-col items-center justify-center px-24 w-3/5 mx-auto space-y-5">
+      <main className="flex min-h-screen flex-col items-center justify-center px-6 sm:px-12 md:px-24 w-full sm:w-11/12 md:w-4/5 lg:w-3/5 mx-auto space-y-5">
         { children }
-      </main>
+      </main>  
     </div>
   );
 }
